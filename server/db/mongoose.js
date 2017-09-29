6509fe608ac456847d8851cf5f937efd8cf41ebf
Bilal Waseem <bilalwc42@gmail.com>
2017-09-29 19:26:17 +0500

    Added heroku support v2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var con =  mongodb:bilalwaseem:fuhrer121@ds159377.mlab.com:59377/todoapp_dev;
+var con =  'mongodb:bilalwaseem:fuhrer121@ds159377.mlab.com:59377/todoapp_dev';
 mongoose.connect(con || 'mongodb://localhost:27017/TodoApp', { useMongoClient: true });
 
 module.exports = {mongoose};
