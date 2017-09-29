e725442190e2d7234dcdc4da7f49b7424e2fdcd9
Bilal Waseem <bilalwc42@gmail.com>
2017-09-29 21:39:38 +0500

    Final version of the API

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var con =  'mongodb://bilaldb:mongolab@ds159377.mlab.com:59377/todoapp_dev';
-// var loc = 'mongodb://localhost:27017/TodoApp';
-mongoose.connect(con, { useMongoClient: true });
+// var con =  'mongodb://bilaldb:mongolab@ds159377.mlab.com:59377/todoapp_dev';
+var loc = 'mongodb://localhost:27017/TodoApp';
+mongoose.connect(loc, { useMongoClient: true });
 
 module.exports = {mongoose};
